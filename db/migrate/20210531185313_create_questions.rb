09f6fa37954193912e4ebaa6585d89e3dfea7550
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-06-01 19:18:09 +0300

    Part3 fix 3

--- db/migrate/20210531185313_create_questions.rb
+++ db/migrate/20210531185313_create_questions.rb
@@ -1,7 +1,6 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-      t.integer :number, null: false
       t.string :description, null: false
       t.references :test, null: false, foreign_key: true
 
