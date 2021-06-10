4ea1a4ed5aae07a1ca7256c630f0420143ce3174
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-06-10 21:06:33 +0000

    Mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   def complete_tests(level = 0)
     Test
-      .select('tests.title')
       .joins('INNER JOIN results ON results.test_id == tests.id')
       .where(results: { user_id: id }, level: level)
   end
