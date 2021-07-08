01b33324d5cfde02baf84be941e758cb0e9c3ed3
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-07-08 09:13:31 +0000

    Add answers. Add admin permissions

--- db/migrate/20210531185605_create_users.rb
+++ db/migrate/20210531185605_create_users.rb
@@ -1,11 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name, null: false
+      t.string :nickname, null: false
+      t.string :type, null: false, default: 'User'
+      t.string :first_name
+      t.string :last_name
       t.string :email, null: false
       t.string :password_digest, null: false
 
       t.timestamps
     end
+
+    add_index :users, :type
   end
 end
