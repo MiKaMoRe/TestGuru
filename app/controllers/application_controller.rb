e99ac448cdca808961d6fa90e2e763427c770bc7
MiKaMoRe <kim.roman-2002@yandex.ru>
2021-07-09 14:53:01 +0000

    Add flash greeting into sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    if current_user.first_name
-      flash[:notice] = "Привет, #{current_user.first_name} #{current_user.last_name}"
-    end
     admin_tests_path if resource.is_a?(Admin)
     tests_path
   end
